2509e8d0cb9eab38a6e0dd5010074fbe799f7aad
syarifmathis <syarif.mathis@gmail.com>
2017-12-01 00:34:06 +0100

    Aufgabe 1 &2

--- Mensa/src/Mensa.java
+++ Mensa/src/Mensa.java
@@ -14,29 +14,24 @@
 */
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.PriorityQueue;
 
 class Mensa {
-    private PriorityQueue<String> buffet;
+    private PriorityQueue<String> buffet = new PriorityQueue<>();
 
-
-
-
-
-
-
-    void put(String[] gerichte){
-
+    synchronized void put(String[] gerichte){
+        // Fett giessen, eine sekunde warten
+        System.out.println ("(" + System.currentTimeMillis() + ") Mensa: Ah, der Nachschub ist da. Noch ein bisschen Fett darueber giessen...");
         try {
-            System.out.println ("(" + System.currentTimeMillis() + ") Mensa: Ah, der Nachschub ist da. Noch ein bisschen Fett darueber giessen...");
             wait(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }finally {
-            notify();
-            buffet.addAll(Arrays.asList(gerichte));
         }
+
+        // Essen in auf das Buffet stellen.
+        buffet.addAll(Arrays.asList(gerichte));
+        System.out.println ("(" + System.currentTimeMillis()+") Mensa: Neue Gerichte bereit. Koennt alle kommen!");
+
 
         while (buffet.size() >= 10){
             try {
@@ -45,29 +40,45 @@ class Mensa {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            if (buffet.size()<= 3){
+                notify();
+                System.out.println ("(" + System.currentTimeMillis() + ") Mensa: !Aufwachen!");
+            }
         }
-        notify();
-
-
     }
 
     synchronized String get(int id){
-        if (buffet.size()<= 3){
-            notify();
-            System.out.println ("(" + System.currentTimeMillis() + ") Mensa: !Aufwachen!");
-        }
+
         while (buffet.isEmpty()){
             System.out.println ("(" + System.currentTimeMillis() + ") " +"Mensa: Nichts mehr da, Herr Prof " + id + ", sie muessen warten!");
-
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         notify();
-        System.out.println ("(" + System.currentTimeMillis()+") Mensa: Neue Gerichte bereit. Koennt alle kommen!");
 
         String gericht=buffet.poll();
-        System.out.println ("(" + System.currentTimeMillis() + ") Mensa: Voila, einmal " + "[ausgabe]" + " fuer Prof " + id + ".");
-        System.out.println ("(" + System.currentTimeMillis() + ") Mensa: Noch " + "[vorrat.size()]" + " Gerichte uebrig.");
+        System.out.println ("(" + System.currentTimeMillis() + ") Mensa: Voila, einmal " + gericht + " fuer Prof " + id + ".");
+        System.out.println ("(" + System.currentTimeMillis() + ") Mensa: Noch " + buffet.size() + " Gerichte uebrig.");
         return gericht;
-
-
     }
 }
+
+// get and put methods should be marked synchronized to make them threadsafe. Besides wait and notify can only be called in a synchronized block.
+
+// start() does not call the run method. instead it starts the thread which executes the run method.
+// If you call run() directly you don't start the thread, you just execute the method on the same caller method.
+// taken from StackOverflow
+// So basically we use start() to start a new Thread. The Threads will then call the run() method, to perform their own tasks.
+
+// in case where the buffet is full, we wait until the food is consumed by the professor. If buffet is almost empty, we start to fill the buffet until
+// a certain limit is reached.
+
+// The class Mensa is the monitoring entity.
+
+// TASK 2
+// This implementation is not necessarily fair, because perfect fairness means: all threads should be advancing their work in almost equal portions;
+// We can make achieve fairness by setting priority based on waiting time. Thread that has been waiting the longest will have the highest priority.
+
