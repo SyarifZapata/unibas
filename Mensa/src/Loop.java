6bd1dbd53f48a86007f91098ace5a90b0b8bf251
syarif <syarif.hidayatullah@stud.unibas.ch>
2017-12-01 15:43:46 +0100

    aufgabe4

--- Mensa/src/Loop.java
+++ Mensa/src/Loop.java
@@ -1,18 +1,23 @@
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 /**
- * Created by Arkad on 12/1/2017.
+ * Created by Syarif on 12/1/2017.
  */
 class Loop {
     static long counter;
 
     public static void main(String[] args) {
+        Random random = new Random();
+        int id = random.nextInt(10);
+    
+    	// Print out counter every second
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                System.out.println(counter);
+                System.out.println("programm " + id+": " + counter);
             }
         }, 0, 1000);
 
@@ -22,3 +27,7 @@ class Loop {
     }
 
 }
+
+// you can manipulate the process priority using nice -n <-19 upto 19>
+// The results differ, sometimes the difference is significant, sometimes there is
+// only small difference
